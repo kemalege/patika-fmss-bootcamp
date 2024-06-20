f4d6df66b3f24c951b19f1b6f7e5359d2d572044
kemalege <cirpanliege@gmail.com>
2024-06-20 19:03:11 +0300

    Add method findInvoicesByCustomerId

--- week-3/src/main/java/org/example/repository/InvoiceRepository.java
+++ week-3/src/main/java/org/example/repository/InvoiceRepository.java
@@ -1,9 +1,12 @@
 package org.example.repository;
 
+import org.example.model.Customer;
 import org.example.model.Invoice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class InvoiceRepository {
     private List<Invoice> invoiceList = new ArrayList<>();
@@ -14,5 +17,11 @@ public class InvoiceRepository {
 
     public List<Invoice> getAll() {
         return invoiceList;
+    }
+
+    public List<Invoice> findInvoicesByCustomerId(Long id) {
+        return getAll().stream()
+                .filter(invoice -> invoice.getCustomerId().equals(id))
+                .collect(Collectors.toList());
     }
 }
